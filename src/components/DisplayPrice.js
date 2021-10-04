4bce9809776210e2754741018bd2faa75359e906
figs <a.figs@protonmail.com>
2021-10-03 20:29:16 -0700

    Implemented DisplayPrice component. Displays the bitcoin price on the main page

--- src/components/DisplayPrice.js
+++ src/components/DisplayPrice.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function BitcoinPrice() {
-    const response = fetch('https://api.nomics.com/v1/currencies/ticker?key={your key here}3&ids=BTC');
-    const myJson = response.json;
-    console.log(myJson);
 
-    return (
-        <div>
-            <h2>Bitcoin price is {myJson}</h2>
-        </div>
-    )
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch("https://api.nomics.com/v1/currencies/ticker?key={your-key-here}&ids=BTC")
+                .then(res => res.json())
+                .then(
+                    (result) => {
+                        setIsLoaded(true);
+                        setItems(result);
+                    },
+                    (error) => {
+                        setIsLoaded(true);
+                        setError(error);
+                    }
+                )
+    }, [])
+
+    if(error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        return (
+            <div>
+                {items.map(item => (
+                    <div key={item.id}>
+                        {item.price}
+                    </div>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default BitcoinPrice;
-
-// fetch("https://api.nomics.com/v1/currencies/ticker?key=your-key-here&ids=BTC,ETH,XRP&interval=1d,30d&convert=EUR&per-page=100&page=1")
-//   .then(response => response.json())
-//   .then(data => console.log(data))
