1ceec880e59c22a924ae7079071d0e1d01946cff
figs <a.figs@protonmail.com>
2021-10-07 23:07:26 -0700

    Calling the API that gets the current bitcoin price in App.js and sending that value as a prop to the components that need it.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import './App.css';
 import './components/DisplayText';
+import { useEffect, useState } from 'react';
 import Example from './components/DisplayText';
 import BitcoinPrice from './components/DisplayPrice';
 
 function App() {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+  const MINUTE_MS = 60000;
+
+  useEffect(() => {
+    fetch("https://api.nomics.com/v1/currencies/ticker?key=&ids=BTC")
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoaded(true);
+        setItems(result);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  }, []),
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("Updates every minute.");
+    }, MINUTE_MS);
+
+    return () => clearInterval(interval);
+  }, [])
+
   return (
     <div className="App">
       <h1>Yooo</h1>
-      <Example />
-      <BitcoinPrice />
+      <BitcoinPrice error={error} isLoaded={isLoaded} items={items} />
     </div>
   );
 }
--- src/components/DisplayPrice.js
+++ src/components/DisplayPrice.js
@@ -1,26 +1,4 @@
-import React, { useEffect, useState } from 'react';
-
-function BitcoinPrice() {
-
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [items, setItems] = useState([]);
-
-    useEffect(() => {
-        fetch("https://api.nomics.com/v1/currencies/ticker?key={your-key-here}&ids=BTC")
-                .then(res => res.json())
-                .then(
-                    (result) => {
-                        setIsLoaded(true);
-                        setItems(result);
-                    },
-                    (error) => {
-                        setIsLoaded(true);
-                        setError(error);
-                    }
-                )
-    }, [])
-
+const BitcoinPrice = ({ error, isLoaded, items}) => {
     if(error) {
         return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
