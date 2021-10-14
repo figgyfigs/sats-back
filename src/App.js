4397e28aa8ee89e6183189bf75b032612b5185db
figs <a.figs@protonmail.com>
2021-10-14 09:32:11 -0700

    Price is updating correctly. Cleaned up some code.

--- src/App.js
+++ src/App.js
@@ -9,45 +9,28 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [items, setItems] = useState([]);
 
-  // useEffect(() => {
-  //   fetch("https://api.nomics.com/v1/currencies/ticker?key=&ids=BTC")
-  //     .then(res => res.json())
-  //     .then((result) => {
-  //       setIsLoaded(true);
-  //       setItems(result);
-  //     },
-  //     (error) => {
-  //       setIsLoaded(true);
-  //       setError(error);
-  //     }
-  //   )
-  // }, []);
+  const getAPIData = useCallback(async () => {
+    fetch('https://api.nomics.com/v1/currencies/ticker?key=&ids=BTC')
+      .then(res => res.json())
+      .then((result) => {
+        setIsLoaded(true);
+        setItems(result)
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      })
+  }, []);  
 
-  // function getPrice() {
-  //   fetch("https://api.nomics.com/v1/currencies/ticker?key=&ids=BTC")
-  //     .then(res => res.json())
-  //     .then((result) => {
-  //       result.map(item => (
-  //         console.log(item.price)
-  //       ))
-  //     })
-  // }
-  
   useEffect(() => {
     
     getAPIData();
 
     const interval = setInterval(() => {
       getAPIData()
-    }, 60000);
-
+    }, 180000);
     return () => clearInterval(interval);
   }, [getAPIData]);
-
-  const getAPIData = useCallback(async () => {
-
-  })
-
 
   return (
     <div className="App">
@@ -58,12 +41,3 @@ function App() {
 }
 
 export default App;
-
-
-// useEffect(() => {
-//   const interval = setInterval(() => {
-//     console.log("Updates every minute.");
-//   }, MINUTE_MS);
-
-//   return () => clearInterval(interval);
-// }, [])
